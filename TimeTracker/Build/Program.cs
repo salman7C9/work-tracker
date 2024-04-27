133bd89566641be7915bed39b22c282398f1a5c9
Noxsense <noxsense@gmail.com>
2024-04-27 20:43:54 +0200

    Follow Microsoft's Unit Testing C# in .NET Guide
    See https://learn.microsoft.com/en-us/dotnet/core/testing/unit-testing-with-dotnet-test

---
+++ TimeTracker/Build/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Build
+{
+    public class Program
+    {
+        public bool Foo(int bar)
+        {
+            Console.WriteLine("Hello, World, I am the new TimeTracker Project!");
+            throw new NotImplementedException("Not implemented.");
+        }
+    }
+}
