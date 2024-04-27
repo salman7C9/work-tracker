1564b5516ab059dea5ef070ab172d5c939673b79
Noxsense <noxsense@gmail.com>
2024-04-27 21:08:47 +0200

    TDD: Implement Test requirements
    if bar = 4 then false

--- TimeTracker/Build/Program.cs
+++ TimeTracker/Build/Program.cs
@@ -9,7 +9,13 @@
         public bool Foo(int bar)
         {
             Console.WriteLine("Hello, World, I am the new TimeTracker Project!");
-            throw new NotImplementedException("Not implemented.");
+
+            if (bar == 4)
+            {
+                return false;
+            }
+
+            throw new NotImplementedException("Not fully implemented.");
         }
     }
 }
