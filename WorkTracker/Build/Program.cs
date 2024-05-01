bea56d90f4ba050cc60a2581a6ddc5ad7a1b2328
Salman Alhadziev <salman.alhadziev@gmail.com>
2024-05-02 00:16:06 +0200

    removed unused imports

--- WorkTracker/Build/Program.cs
+++ WorkTracker/Build/Program.cs
@@ -1,6 +1,4 @@
-﻿using System.Threading;
-
-namespace Build
+﻿namespace Build
 {
     public class Program
     {
--- WorkTracker/Build/WorkTimeRecord.cs
+++ WorkTracker/Build/WorkTimeRecord.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Dynamic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Build
+﻿namespace Build
 {
     public class WorkTimeRecord
     {
